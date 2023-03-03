e444dcbec8626ef1838abfa49462d0acdb2f7977
JohnnyOak85 <joao.cmp@proton.me>
2023-03-03 22:25:57 +0000

    Battle logic progress

--- game/helpers/battle.ts
+++ game/helpers/battle.ts
@@ -1,6 +1,8 @@
 import { DiscordChannel } from '../../commons/discord';
 import { Log } from '../../commons/tools';
 import { getPlayer, PlayerData } from './player';
+import gameStore from './stores/game.store';
+import messageStore from './stores/message.store';
 import monsterStore from './stores/monster.store';
 
 export const engageBattle = async (playerData: PlayerData, channel: DiscordChannel) => {
@@ -8,8 +10,12 @@ export const engageBattle = async (playerData: PlayerData, channel: DiscordChann
         const monster = monsterStore.getMonster();
         const player = await getPlayer(playerData);
 
-        console.log(monster);
         if (!monster) return;
+
+        gameStore.stop();
+        messageStore.deleteMessage();
+
+        // gameStore.restart();
     } catch (error) {
         Log.error(error, 'engageBattle');
         throw error;
