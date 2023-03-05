8392ee8b3046e642d398e4cc088493b59caf51eb
JohnnyOak85 <joao.cmp@proton.me>
2023-03-05 18:03:03 +0000

    Reminders refactor

--- reminder/helpers/reminder.ts
+++ reminder/helpers/reminder.ts
@@ -1,5 +1,5 @@
 import Discord, { DiscordGuild, DiscordMember } from '../../commons/discord';
-import { Api, Gamble, Schedule } from '../../commons/tools';
+import { Api, Gamble, Log, Schedule, Time } from '../../commons/tools';
 
 type ReminderConfig = {
     birthdays: string[];
@@ -22,79 +22,90 @@ const API_URL = 'reminders';
 
 const remindersList: string[] = [];
 
-async function buildBirthdayEmbed({ nickname, avatarURL, user: { username } }: DiscordMember) {
+const getBirthdayMessage = async (nickname: string) => {
     const { birthdays } = await Api.get<ReminderConfig>(`${API_URL}/config`);
 
-    return {
-        color: 0xffd700,
-        fields: {
-            name: '\u200B',
-            value: birthdays[Gamble.randomIndex(birthdays)].replace(
-                '§nickname',
-                nickname || username
-            )
-        },
-        thumbnail: avatarURL(),
-        title: 'HAPPY ANNIVERSARY!',
-        url: 'https://www.youtube.com/watch?v=8zgz2xBrvVQ'
-    };
-}
+    return birthdays[Gamble.randomIndex(birthdays)].replace('§nickname', nickname);
+};
 
-function buildCelebrationEmbed({ event, image }: ReminderEmbed) {
-    return {
-        color: 0x0dff00,
-        fields: { name: '\u200B', value: `Today is ${event}` },
-        image,
-        title: 'CELEBRATION DAY!'
-    };
-}
+const buildBirthdayEmbed = async ({ nickname, avatarURL, user: { username } }: DiscordMember) => ({
+    color: 0xffd700,
+    fields: {
+        name: '\u200B',
+        value: await getBirthdayMessage(nickname || username)
+    },
+    thumbnail: avatarURL(),
+    title: 'HAPPY ANNIVERSARY!',
+    url: 'https://www.youtube.com/watch?v=8zgz2xBrvVQ'
+});
 
-function buildReleaseEmbed({ image, url }: ReminderEmbed) {
-    return {
-        color: 0xff0000,
-        fields: { name: '\u200B', value: '\u200B' },
-        title: 'RELEASE DAY!',
-        image,
-        url
-    };
-}
+const buildCelebrationEmbed = ({ event, image }: ReminderEmbed) => ({
+    color: 0x0dff00,
+    fields: { name: '\u200B', value: `Today is ${event}` },
+    image,
+    title: 'CELEBRATION DAY!'
+});
+
+const buildReleaseEmbed = ({ image, url }: ReminderEmbed) => ({
+    color: 0xff0000,
+    fields: { name: '\u200B', value: '\u200B' },
+    title: 'RELEASE DAY!',
+    image,
+    url
+});
+
+const markAsDone = (reminder: ReminderEmbed) => {
+    reminder.done = true;
+
+    Api.put(API_URL, reminder);
+};
 
 const runsOnce = (type: string) => !YEARLY_EVENTS.includes(type);
 
 export default async ({ members, systemChannel }: DiscordGuild) => {
-    const reminders = await Api.get<ReminderEmbed[]>(API_URL);
+    try {
+        const reminders = await Api.get<ReminderEmbed[]>(API_URL);
 
-    for (const reminder of reminders) {
-        if (remindersList.includes(reminder._id)) {
-            continue;
-        }
-
-        const callBack = async () => {
-            switch (reminder.type) {
-                case 'birthday':
-                    systemChannel?.send(
-                        Discord.buildEmbed(
-                            await buildBirthdayEmbed(await members.fetch(reminder.memberId || ''))
-                        )
-                    );
-                    break;
-                case 'celebration':
-                    systemChannel?.send(Discord.buildEmbed(buildCelebrationEmbed(reminder)));
-                    break;
-                case 'release':
-                    systemChannel?.send(Discord.buildEmbed(buildReleaseEmbed(reminder)));
-
-                    reminder.done = true;
-
-                    Api.put(API_URL, reminder);
-                    break;
+        for (const reminder of reminders) {
+            if (remindersList.includes(reminder._id) || reminder.done) {
+                continue;
             }
-        };
 
-        runsOnce(reminder.type)
-            ? Schedule.runOnce(reminder.timestamp, callBack)
-            : Schedule.run(reminder.timestamp, callBack);
+            if (Time.getTimestamp() > reminder.timestamp) {
+                markAsDone(reminder);
+                continue;
+            }
 
-        remindersList.push(reminder._id);
+            const callBack = async () => {
+                switch (reminder.type) {
+                    case 'birthday':
+                        systemChannel?.send(
+                            Discord.buildEmbed(
+                                await buildBirthdayEmbed(
+                                    await members.fetch(reminder.memberId || '')
+                                )
+                            )
+                        );
+                        break;
+                    case 'celebration':
+                        systemChannel?.send(Discord.buildEmbed(buildCelebrationEmbed(reminder)));
+                        break;
+                    case 'release':
+                        systemChannel?.send(Discord.buildEmbed(buildReleaseEmbed(reminder)));
+
+                        markAsDone(reminder);
+                        break;
+                }
+            };
+
+            runsOnce(reminder.type)
+                ? Schedule.runOnce(reminder.timestamp, callBack)
+                : Schedule.run(reminder.timestamp, callBack);
+
+            remindersList.push(reminder._id);
+        }
+    } catch (error) {
+        Log.error(error, 'startReminders');
+        throw error;
     }
 };
