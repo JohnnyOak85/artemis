d49201e1bc41f67d53f3fb921b8dbbdd479d5b41
JohnnyOak85 <joao.cmp@proton.me>
2023-03-05 17:56:36 +0000

    Adding timestamp to time helper.

--- commons/tools/time.ts
+++ commons/tools/time.ts
@@ -3,5 +3,6 @@ import moment from 'moment';
 const TIMESTAMP_FORMAT = 'YYYY-MM-DD HH:mm:ss:SSS';
 
 export default {
-    get: () => moment().format(TIMESTAMP_FORMAT)
+    get: () => moment().format(TIMESTAMP_FORMAT),
+    getTimestamp: () => moment().valueOf()
 };
