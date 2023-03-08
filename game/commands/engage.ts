185935bcc5c5a2abc2c2e689168c309753b2a549
JohnnyOak85 <joao.cmp@proton.me>
2023-03-08 09:14:40 +0000

    Command verification

--- game/commands/engage.ts
+++ game/commands/engage.ts
@@ -1,11 +1,23 @@
 import Discord, { DiscordChannel, DiscordCommandInteraction } from '../../commons/discord';
+import { Environment } from '../../commons/tools';
 import { engageBattle } from '../helpers/battle';
 
+const commandData = {
+    description: 'Attack a spawned monster! (Game Category Exclusive)',
+    dmPermission: false,
+    name: 'engage'
+};
+
 export default {
-    data: Discord.buildCommand('engage', 'Attack a spawned monster!'),
+    data: Discord.buildCommand(commandData),
     execute: async (interaction: DiscordCommandInteraction) => {
-        if (interaction.channel?.type !== Discord.ChannelTypes.text) {
-            return interaction.reply('I cannot oblige');
+        const { game } = Environment.get();
+
+        if (
+            interaction.channelId !== game ||
+            interaction.channel?.type !== Discord.ChannelTypes.text
+        ) {
+            return interaction.reply('I cannot oblige'); // TODO Get special random replies
         }
 
         const roles = Discord.getPlayerRoles(interaction.member);
--- game/commands/toggle.ts
+++ game/commands/toggle.ts
@@ -1,8 +1,23 @@
 import Discord, { DiscordCommandInteraction } from '../../commons/discord';
+import { Environment } from '../../commons/tools';
 import toggler from '../helpers/toggler';
 
+const commandData = {
+    description: 'Toggle the game on or off. (Mod && Game Category Exclusive)',
+    dmPermission: false,
+    memberPermissions: Discord.Permissions.admin,
+    name: 'toggle'
+};
+
 export default {
-    data: Discord.buildCommand('toggle', 'Toggle the game on or off.'),
-    execute: async (interaction: DiscordCommandInteraction) =>
-        await interaction.reply(await toggler(interaction.guild))
+    data: Discord.buildCommand(commandData),
+    execute: async (interaction: DiscordCommandInteraction) => {
+        const { game } = Environment.get();
+
+        if (interaction.channelId !== game) {
+            return interaction.reply('I cannot oblige'); // TODO Get special random replies
+        }
+
+        await interaction.reply(await toggler(interaction.guild));
+    }
 };
