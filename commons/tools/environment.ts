ff71e184e4f7093fefa095d6e50131980c35510b
JohnnyOak85 <joao.cmp@proton.me>
2023-03-24 22:17:11 +0000

    Removing suffix

--- commons/tools/environment.ts
+++ commons/tools/environment.ts
@@ -4,7 +4,7 @@ import paths from '../paths';
 
 export default {
     get: () => ({
-        api: `${process.env.API_URL}/${process.env.API_SUFFIX}` || '',
+        api: `${process.env.API_URL}` || '',
         game: process.env.GAME_CHANNEL || '',
         general: process.env.GENERAL_CHANNEL || '',
         guild: process.env.GUILD_ID || '',
